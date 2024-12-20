b81038f486da3378c9640aae9e96fc98fdb022b0
awani-rawat <45591355+awani-rawat@users.noreply.github.com>
2024-12-20 18:38:53 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,9 @@ st.write(
 #session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name = st.text_input('Name on Smoothie:')
 st.write('The name on smoothie will be: ', name)
